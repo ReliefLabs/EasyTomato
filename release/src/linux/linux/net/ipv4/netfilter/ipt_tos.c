787e1c2577fc5e61aee6460a1e698226069afda9
DJFurie <djfurie@relieflabs.org>
2012-05-17 01:26:20 -0400

    Pre-merge commit

--- release/src/linux/linux/net/ipv4/netfilter/ipt_tos.c
+++ release/src/linux/linux/net/ipv4/netfilter/ipt_tos.c
@@ -1,95 +1,50 @@
-/* This is a module which is used for setting the TOS field of a packet. */
+/* Kernel module to match TOS values. */
 #include <linux/module.h>
 #include <linux/skbuff.h>
-#include <linux/ip.h>
-#include <net/checksum.h>
 
+#include <linux/netfilter_ipv4/ipt_tos.h>
 #include <linux/netfilter_ipv4/ip_tables.h>
-#include <linux/netfilter_ipv4/ipt_TOS.h>
 
-static unsigned int
-target(struct sk_buff **pskb,
-       unsigned int hooknum,
-       const struct net_device *in,
-       const struct net_device *out,
-       const void *targinfo,
-       void *userinfo)
+static int
+match(const struct sk_buff *skb,
+      const struct net_device *in,
+      const struct net_device *out,
+      const void *matchinfo,
+      int offset,
+      const void *hdr,
+      u_int16_t datalen,
+      int *hotdrop)
 {
-	struct iphdr *iph = (*pskb)->nh.iph;
-	const struct ipt_tos_target_info *tosinfo = targinfo;
+	const struct ipt_tos_info *info = matchinfo;
+	const struct iphdr *iph = skb->nh.iph;
 
-	if ((iph->tos & IPTOS_TOS_MASK) != tosinfo->tos) {
-		u_int16_t diffs[2];
-
-		/* raw socket (tcpdump) may have clone of incoming
-                   skb: don't disturb it --RR */
-		if (skb_cloned(*pskb) && !(*pskb)->sk) {
-			struct sk_buff *nskb = skb_copy(*pskb, GFP_ATOMIC);
-			if (!nskb)
-				return NF_DROP;
-			kfree_skb(*pskb);
-			*pskb = nskb;
-			iph = (*pskb)->nh.iph;
-		}
-
-		diffs[0] = htons(iph->tos) ^ 0xFFFF;
-		iph->tos = (iph->tos & IPTOS_PREC_MASK) | tosinfo->tos;
-		diffs[1] = htons(iph->tos);
-		iph->check = csum_fold(csum_partial((char *)diffs,
-		                                    sizeof(diffs),
-		                                    iph->check^0xFFFF));
-		(*pskb)->nfcache |= NFC_ALTERED;
-	}
-	return IPT_CONTINUE;
+	return (iph->tos == info->tos) ^ info->invert;
 }
 
 static int
 checkentry(const char *tablename,
-	   const struct ipt_entry *e,
-           void *targinfo,
-           unsigned int targinfosize,
+           const struct ipt_ip *ip,
+           void *matchinfo,
+           unsigned int matchsize,
            unsigned int hook_mask)
 {
-	const u_int8_t tos = ((struct ipt_tos_target_info *)targinfo)->tos;
-
-	if (targinfosize != IPT_ALIGN(sizeof(struct ipt_tos_target_info))) {
-		printk(KERN_WARNING "TOS: targinfosize %u != %Zu\n",
-		       targinfosize,
-		       IPT_ALIGN(sizeof(struct ipt_tos_target_info)));
+	if (matchsize != IPT_ALIGN(sizeof(struct ipt_tos_info)))
 		return 0;
-	}
-
-	if (strcmp(tablename, "mangle") != 0) {
-		printk(KERN_WARNING "TOS: can only be called from \"mangle\" table, not \"%s\"\n", tablename);
-		return 0;
-	}
-
-	if (tos != IPTOS_LOWDELAY
-	    && tos != IPTOS_THROUGHPUT
-	    && tos != IPTOS_RELIABILITY
-	    && tos != IPTOS_MINCOST
-	    && tos != IPTOS_NORMALSVC) {
-		printk(KERN_WARNING "TOS: bad tos value %#x\n", tos);
-		return 0;
-	}
 
 	return 1;
 }
 
-static struct ipt_target ipt_tos_reg
-= { { NULL, NULL }, "TOS", target, checkentry, NULL, THIS_MODULE };
+static struct ipt_match tos_match
+= { { NULL, NULL }, "tos", &match, &checkentry, NULL, THIS_MODULE };
 
 static int __init init(void)
 {
-	if (ipt_register_target(&ipt_tos_reg))
-		return -EINVAL;
-
-	return 0;
+	return ipt_register_match(&tos_match);
 }
 
 static void __exit fini(void)
 {
-	ipt_unregister_target(&ipt_tos_reg);
+	ipt_unregister_match(&tos_match);
 }
 
 module_init(init);
