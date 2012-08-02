51ff7bd43680a4fa4188c4168db8f6ffa7f782d3
DJFurie <djfurie@relieflabs.org>
2012-08-01 21:50:08 -0700

    Some more updates to the thumbdrive setup.

--- release/src/router/easytomato/format_thumbdrive.sh
+++ release/src/router/easytomato/format_thumbdrive.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+EASYOPT_FILE=EasyOptionals_ALPHA_0.1.tar.gz
+
+# Unmount the drive if it was already mounted
+umount /dev/sda1
+umount /dev/sda2
+umount /dev/sda3
+umount /dev/sda4
+
 # First, partition the drive
 echo "d
 1
@@ -13,28 +21,23 @@ n
 p
 1
 
-+13000M
-n
-p
-2
-
 
 a
 1
 w
 " | fdisk /dev/sda
 
-# Second, format the first partition (/dev/sda1)
-mkfs.ext3 /dev/sda1
-
-# Third, format the second partition (/dev/sda2)
-mkfs.vfat /dev/sda2
-
+# Format the first partition (/dev/sda1)
+mkfs.ext3 -L EasyOptionals /dev/sda1
 
 # Do a mount of the newly created file systems here
-
+umount /dev/sda1
+mount -t ext3 -w /dev/sda1 /easytomato/easyoptionals
 
 # Download the easyoptionals.tar.gz package from some webserver
+cd /easytomato/easyoptionals
+wget http://dl.dropbox.com/u/91016047/Squid%20Tar/$EASYOPT_FILE
 
 # Extract the easyoptionals.tar.gz package to /easytomato/easyoptionals/
-
+tar -xzf $EASYOPT_FILE
+rm $EASYOPT_FILE
