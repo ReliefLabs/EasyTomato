54912194612e4588f05668c889f026cec8257db1
Sonia Fabre <soniaf@trustyou.com>
2012-11-16 16:09:24 -0500

    update computers table according to time range

--- release/src/router/www/js/computers.js
+++ release/src/router/www/js/computers.js
@@ -8,6 +8,35 @@ function updateScale(hours) {
 	txGraph.xDomain([then, now]);
 	rxGraph.xDomain([then, now]);
 
+	setTimeout(function(){speedHistorySubset(hours)},300);
+
+}
+function speedHistorySubset(hours) {
+
+	for (ip in speed_history) {
+
+		// if ip is not a subnet
+		if (!/\.0$/.test(ip)) { 
+			var rx_slice = speed_history[ip].rx.slice(
+				speed_history[ip].rx.length-(hours*30),
+				speed_history[ip].rx.length-1
+			),
+			tx_slice = speed_history[ip].tx.slice(
+				speed_history[ip].tx.length-(hours*30),
+				speed_history[ip].tx.length-1
+			),
+			rx_sum = _.reduce(rx_slice, function(memo, num){ return memo + num; }, 0),
+			tx_sum = _.reduce(tx_slice, function(memo, num){ return memo + num; }, 0);
+			
+			// Update table data object
+			var perComputerDataRow = _.find(perComputerData, function(item){ return item.ip == ip; });
+			perComputerDataRow.rx_total = rx_sum;
+			perComputerDataRow.tx_total = tx_sum;
+		}
+	};
+	
+	d3.select("#table").datum(perComputerData).call(dataTable);
+
 }
 
 $(document).ready(function() {
@@ -17,4 +46,4 @@ $(document).ready(function() {
 		var hours = $(this).attr('data-hours');
 		updateScale(hours);
 	});
-})
+});
